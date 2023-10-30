d32df6b191c98a765021f7ab412e2d67e34c46b3
Fellipe Nogueira Milomem <fellipemilomem@hotmail.com>
2023-10-30 18:48:44 -0300

    Ignite-DTMoney

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,49 @@
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHighLight,
+  TransactionContainer,
+  TransactionTable,
+} from './styles'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/fomatter'
+import { useContextSelector } from 'use-context-selector'
+
+export function Transactions() {
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
+
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionContainer>
+        <SearchForm />
+        <TransactionTable>
+          <tbody>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="40%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
+    </div>
+  )
+}
